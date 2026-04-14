9aa5850aac14cb124a874bd92f88e18e49e9a562
Carlos Alberto Giralt Torriente <cagiraltt@gmail.com>
2026-04-14 11:30:50 -0300

    feat(gallery): add custom toolbar actions for download functionality in demo galleries

--- src/ui/index.ts
+++ src/ui/index.ts
@@ -2,3 +2,9 @@ import "./panzoom/lk-panzoom";
 import "./carousel/lk-carousel";
 import "./gallery/lk-gallery-toolbar";
 import "./gallery/lk-gallery";
+
+export type {
+	CustomToolContext,
+	CustomToolRenderer,
+} from "./gallery/lk-gallery-toolbar.js";
+export { GalleryToolbar } from "./gallery/lk-gallery-toolbar.js";
